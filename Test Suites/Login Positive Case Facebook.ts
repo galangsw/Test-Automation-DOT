d5b6399c78a932cd309c7fba6cbc556a15c2e5eb
galang <galangsatria27@gmail.com>
2023-01-05 13:27:48 +0700

    Make Login Test Case and Test Suite

---
+++ Test Suites/Login Positive Case Facebook.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Positive Case Facebook</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6d900c2d-ee7a-48cf-9dc2-bb13bd59c952</testSuiteGuid>
+   <testCaseLink>
+      <guid>3ae295e3-8160-45ea-a031-b37b874099e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Go to Facebook</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71897db0-f652-4bfe-8dc0-9a5dee205682</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login Success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ec621d3c-81d9-41f3-b2be-71ffbb2c1661</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3eb103e0-f093-4ad0-a1c9-c7e206e7b030</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
